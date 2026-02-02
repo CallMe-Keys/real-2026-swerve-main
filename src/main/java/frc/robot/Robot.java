11d82af2abf612c3d3f8bdd05e057a1ef637a50d
CallMe-Keys <ucasd7@gmail.com>
2026-02-02 10:27:54 -0500

    add comments to robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,34 +3,42 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
+// import wpilib apis
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
+// creates the command run during autonomous
   private Command m_autonomousCommand;
 
+// manages subsystems and inputs
   private final RobotContainer m_robotContainer;
 
+// makes a new period of managing inputs
   public Robot() {
     m_robotContainer = new RobotContainer();
   }
 
+// overrides the current state and checks for different states about every 20 ms
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run(); 
   }
 
+//resets states or vars
   @Override
   public void disabledInit() {}
 
+//used for monitoring or debugging
   @Override
   public void disabledPeriodic() {}
 
+//reset settings
   @Override
   public void disabledExit() {}
 
+//recieves auton code from m_robotContainer and schedules it as long as auton is active
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
@@ -40,12 +48,15 @@ public class Robot extends TimedRobot {
     }
   }
 
+//used to monitor auton or add logic
   @Override
   public void autonomousPeriodic() {}
 
+//resets auton operation
   @Override
   public void autonomousExit() {}
 
+//cancels auton commands if its still active and lets us shine
   @Override
   public void teleopInit() {
     if (m_autonomousCommand != null) {
@@ -53,23 +64,29 @@ public class Robot extends TimedRobot {
     }
   }
 
+//where driver inputs are processed
   @Override
   public void teleopPeriodic() {}
 
+// used to exit teleop and reset settings and all states
   @Override
   public void teleopExit() {}
 
+//cancels all commands, used for testing only
   @Override
   public void testInit() {
     CommandScheduler.getInstance().cancelAll();
   }
 
+//processes inputs
   @Override
   public void testPeriodic() {}
 
+//exits and resets
   @Override
   public void testExit() {}
 
+//simulates inputs with no bot
   @Override
   public void simulationPeriodic() {}
 }
